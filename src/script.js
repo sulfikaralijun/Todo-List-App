41411912d31578d740f6b78868237738e9d9caf4
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-01-09 21:17:00 +0800

    update index.html, script.js and style.css

--- src/script.js
+++ src/script.js
@@ -1,29 +1,38 @@
 const input = document.getElementById("text"),
-	btn = document.querySelector("button");
-  taskElement = document.querySelector(".tasks");
+	btn = document.querySelector("button"),
+	taskElement = document.querySelector(".tasks");
 
 const tasks = [
-  "Learn HTML",
-  "Learn CSS",
-  "Learn JS",
-  "Learn Bootstrap",
-  "Learn Sass",
-  "Learn Angular",
-  "Learn React",
-  "Learn Vue",
-  "Learn Node",
-  "Learn MongoDB",
-  "Learn Firebase",
-  "Learn GraphQL",
-  "Learn Python",
-  "Learn C++",
-  "Learn C#",
-  "Learn Java",
-  "Learn PHP",
-  "Learn C"
-]
-btn.addEventListener("click", addTask);
+	"Learn HTML",
+	"Learn CSS",
+	"Learn JS",
+	"Learn Bootstrap",
+	"Learn Sass",
+	"Learn Angular",
+	"Learn React",
+	"Learn Vue",
+	"Learn Node",
+	"Learn MongoDB",
+	"Learn Firebase",
+	"Learn GraphQL",
+	"Learn Python",
+	"Learn C++",
+	"Learn C#",
+	"Learn Java",
+	"Learn PHP",
+	"Learn C",
+];
+
+function showTask() {
+	tasks.forEach((task) => {
+		taskElement.innerHTML += `<li>${task}</li>`;
+		console.log(taskElement);
+	});
+}
+
+showTask();
 
 const addTask = () => {
-  input.value
-}
+	input.value;
+};
+btn.addEventListener("click", addTask);
