90077fd2622595d2bbace770f93e6ffca6eb7942
Sulfikar Alijun <sulfikaralijun@gmail.com>
2024-01-10 20:46:24 +0800

    update script.js

--- src/script.js
+++ src/script.js
@@ -8,21 +8,6 @@ const tasks = [
 	"Learn HTML",
 	"Learn CSS",
 	"Learn JS",
-	// "Learn Bootstrap",
-	// "Learn Sass",
-	// "Learn Angular",
-	// "Learn React",
-	// "Learn Vue",
-	// "Learn Node",
-	// "Learn MongoDB",
-	// "Learn Firebase",
-	// "Learn GraphQL",
-	// "Learn Python",
-	// "Learn C++",
-	// "Learn C#",
-	// "Learn Java",
-	// "Learn PHP",
-	// "Learn C",
 ];
 
 // show task in the list
